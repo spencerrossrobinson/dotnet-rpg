e78137c555610ab7b5aa44fccfd53e96a7883baa
spencerrossrobinson <spencerwarmouth@gmail.com>
2023-05-16 15:04:04 -0400

    GetAll and GetId

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+     private static List<Character> characters = new List<Character>
+     {
+        new Character(),
+        new Character {Id = 1, Name = "FUCK"}
+     };   
+
+
+    [HttpGet("GetAll")]
+     public ActionResult<List<Character>> Get() 
+     {
+        return Ok(characters);
+     }
+
+    [HttpGet("{id}")]
+     public ActionResult<Character> GetSingle(int id) 
+     {
+        return Ok(characters.FirstOrDefault( c => c.Id == id));
+     }
+    }
+
+}
