6f0b03ac87a8f1882c5184c8cdc6fa42f4e5fbfa
spencerrossrobinson <spencerwarmouth@gmail.com>
2023-05-17 11:00:01 -0400

    service responses

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
@@ -11,23 +12,31 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-     private static List<Character> characters = new List<Character>
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
      {
-        new Character(),
-        new Character {Id = 1, Name = "FUCK"}
-     };   
+             _characterService = characterService;
+        }
 
 
     [HttpGet("GetAll")]
-     public ActionResult<List<Character>> Get() 
+     public async Task<ActionResult<ServiceResponse<List<Character>>>> Get() 
      {
-        return Ok(characters);
+        return Ok(await _characterService.GetAllCharacters());
      }
 
     [HttpGet("{id}")]
-     public ActionResult<Character> GetSingle(int id) 
+     public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id) 
      {
-        return Ok(characters.FirstOrDefault( c => c.Id == id));
+        return Ok(await _characterService.GetCharacterById(id));
+     }
+
+    [HttpPost]
+     public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+     {
+    
+        return Ok(await _characterService.AddCharacter(newCharacter));
      }
     }
 
---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+    private static List<Character> characters = new List<Character>
+     {
+        new Character(),
+        new Character {Id = 1, Name = "FUCK"}
+     };   
+        public  async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
+        {
+        
+        var serviceResponse = new ServiceResponse<List<Character>>();
+        characters.Add(newCharacter);
+        serviceResponse.Data = characters;
+        return serviceResponse;
+        }
+
+        public  async Task<ServiceResponse<List<Character>>> GetAllCharacters()
+        {
+            var serviceResponse = new ServiceResponse<List<Character>>();
+            serviceResponse.Data = characters;
+            return serviceResponse;
+        }
+
+        public  async Task<ServiceResponse<Character>> GetCharacterById(int id)
+        {
+
+            var serviceResponse = new ServiceResponse<Character>();
+            var character = characters.FirstOrDefault( c => c.Id == id);
+            serviceResponse.Data = character;
+            return serviceResponse;
+        }
+    }
+}
